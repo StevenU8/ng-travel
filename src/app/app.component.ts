051c192b1c60a800988b3ed0cd5936518f38244a
Steven U <Steven U>
2016-12-15 17:28:04 -0800

    Stubbing out landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+
+export class Restaurant {
+  id: string;
+  name: string;
+  imageUrl: string;
+}
+
+const RESTAURANTS: Restaurant[] = [
+  { id: 'restaurant-1', name: 'Restaurant 1', imageUrl: 'https://s3-media3.fl.yelpcdn.com/bphoto/xtqi3PdiT8u_Q3POr8HY5g/258s.jpg' },
+  { id: 'restaurant-2', name: 'Restaurant 2', imageUrl: 'https://s3-media4.fl.yelpcdn.com/bphoto/sEVmjwbp2XOCEqPDc1jBzw/258s.jpg' },
+  { id: 'restaurant-3', name: 'Restaurant 3', imageUrl: 'https://s3-media1.fl.yelpcdn.com/bphoto/0GdASuBGoh3hHJSIkjHSvw/258s.jpg' },
+  { id: 'restaurant-4', name: 'Restaurant 4', imageUrl: 'https://s3-media3.fl.yelpcdn.com/bphoto/4QuySIzlRQi4kGsCOAD80w/258s.jpg' },
+  ];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  trendingRestaurants = RESTAURANTS;
 }
